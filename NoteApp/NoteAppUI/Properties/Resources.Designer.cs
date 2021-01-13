ff9806a14b4bd1000d7c033049506627f5bf11bc
ezcnrmn <47943395+ezcnrmn@users.noreply.github.com>
2021-01-13 16:09:39 +0700

    Создан сценарий сборки установочного пакета

--- NoteApp/NoteAppUI/Properties/Resources.Designer.cs
+++ NoteApp/NoteAppUI/Properties/Resources.Designer.cs
@@ -99,5 +99,15 @@ namespace NoteAppUI.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon icon_256x256 {
+            get {
+                object obj = ResourceManager.GetObject("icon_256x256", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
